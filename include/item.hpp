21422b34157fe81101ae921dd1beb6c82021bed5
Tero Maaranen <tero.maaranen@nokia.com>
2022-04-07 10:37:07 +0300

    Add items.

--- include/item.hpp
+++ include/item.hpp
@@ -24,16 +24,123 @@ class Bona : public Item {
 public:
     Bona(uint32_t x, uint32_t y);
     virtual ~Bona() = default;
-
     virtual std::string typeToChar() const;
     virtual std::string getMsg() const;
+};
+
+class Kalja : public Item {
+public:
+    Kalja(uint32_t x, uint32_t y);
+    virtual ~Kalja() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Lonkka : public Item {
+public:
+    Lonkka(uint32_t x, uint32_t y);
+    virtual ~Lonkka() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Lenkki : public Item {
+public:
+    Lenkki(uint32_t x, uint32_t y);
+    virtual ~Lenkki() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Pamppu : public Item {
+public:
+    Pamppu(uint32_t x, uint32_t y);
+    virtual ~Pamppu() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Veitsi : public Item {
+public:
+    Veitsi(uint32_t x, uint32_t y);
+    virtual ~Veitsi() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Ketjut : public Item {
+public:
+    Ketjut(uint32_t x, uint32_t y);
+    virtual ~Ketjut() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Kivi : public Item {
+public:
+    Kivi(uint32_t x, uint32_t y);
+    virtual ~Kivi() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Bootsit : public Item {
+public:
+    Bootsit(uint32_t x, uint32_t y);
+    virtual ~Bootsit() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Kala : public Item {
+public:
+    Kala(uint32_t x, uint32_t y);
+    virtual ~Kala() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Omppo : public Item {
+public:
+    Omppo(uint32_t x, uint32_t y);
+    virtual ~Omppo() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Banaani : public Item {
+public:
+    Banaani(uint32_t x, uint32_t y);
+    virtual ~Banaani() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+};
+
+class Raha : public Item {
+public:
+    Raha(uint32_t x, uint32_t y);
+    virtual ~Raha() = default;
+    virtual std::string typeToChar() const;
+    virtual std::string getMsg() const;
+    virtual bool interact(PlayerNS::Player* player);
+    uint32_t value;
 };
 
 class Paska : public Item {
 public:
     Paska(uint32_t x, uint32_t y);
     virtual ~Paska() = default;
-
     virtual std::string typeToChar() const;
     virtual std::string getMsg() const;
     virtual bool interact(PlayerNS::Player* player);
--- src/item.cpp
+++ src/item.cpp
@@ -1,12 +1,167 @@
 #include "item.hpp"
 #include "player.hpp"
 #include "level.hpp"
+#include <string>
+
+using std::string;
 
 // bona
 Bona::Bona(uint32_t x, uint32_t y) : Item(x, y) { }
 std::string Bona::typeToChar() const { return "°"; }
 std::string Bona::getMsg() const {
     return "Löysit bonan!";
+}
+
+// kalja
+Kalja::Kalja(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Kalja::typeToChar() const { return "%"; }
+std::string Kalja::getMsg() const {
+    return "Katohhan, miehen mittainen.";
+}
+bool Kalja::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.kalja;
+    return true;
+}
+
+// lonkka
+Lonkka::Lonkka(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Lonkka::typeToChar() const { return "%"; }
+std::string Lonkka::getMsg() const {
+    return "Kappas, lonkan mittainen !!!";
+}
+bool Lonkka::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.lonkka;
+    return true;
+}
+
+// lenkki
+Lenkki::Lenkki(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Lenkki::typeToChar() const { return "("; }
+std::string Lenkki::getMsg() const {
+    return "Nam, lenkki makkaraa.";
+}
+bool Lenkki::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.lenkki;
+    return true;
+}
+
+// pamppu
+Pamppu::Pamppu(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Pamppu::typeToChar() const { return "!"; }
+std::string Pamppu::getMsg() const {
+    return "Pamppu. Mahtaakohan tama olla kadoksissa joltain poltsulta ?";
+}
+bool Pamppu::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.pamput;
+    return true;
+}
+
+// veitset
+Veitsi::Veitsi(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Veitsi::typeToChar() const { return "/"; }
+std::string Veitsi::getMsg() const {
+    return "Teroita veitsesi mummoihin.";
+}
+bool Veitsi::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.veitset;
+    return true;
+}
+
+// ketjut
+Ketjut::Ketjut(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Ketjut::typeToChar() const { return "/"; }
+std::string Ketjut::getMsg() const {
+    return "Kutkuta pollaria riimuilla.";
+}
+bool Ketjut::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.ketjut;
+    return true;
+}
+
+// kivi
+Kivi::Kivi(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Kivi::typeToChar() const { return "."; }
+std::string Kivi::getMsg() const {
+    return "Löysit kiven. Onnitteluni.";
+}
+bool Kivi::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.kivet;
+    return true;
+}
+
+// Bootsit
+Bootsit::Bootsit(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Bootsit::typeToChar() const { return "B"; }
+std::string Bootsit::getMsg() const {
+    return "Camel boots,miehille,jotka kusevat omille jaloileen.";
+}
+bool Bootsit::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.kivet;
+    return true;
+}
+
+// Kala
+Kala::Kala(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Kala::typeToChar() const { return "¥"; }
+std::string Kala::getMsg() const {
+    return "Kalasi lemuaa.";
+}
+bool Kala::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.kalat;
+    return true;
+}
+
+// omppo
+Omppo::Omppo(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Omppo::typeToChar() const { return "¥"; }
+std::string Omppo::getMsg() const {
+    return "Omppo.Sen voi syoda, jos uskaltaa.";
+}
+bool Omppo::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.omppo;
+    return true;
+}
+
+// banaani
+Banaani::Banaani(uint32_t x, uint32_t y) : Item(x, y) { }
+std::string Banaani::typeToChar() const { return ")"; }
+std::string Banaani::getMsg() const {
+    return "Loysit banaanin. Kokeileppas syoda se.";
+}
+bool Banaani::interact(PlayerNS::Player* player)
+{
+    ++player->inventory.bansku;
+    return true;
+}
+
+// Raha
+Raha::Raha(uint32_t x, uint32_t y) : Item(x, y) {
+    value = rand() % 29 + 1;
+}
+std::string Raha::typeToChar() const { return "$"; }
+std::string Raha::getMsg() const {
+    string raha;
+    {
+        char str[10];
+        sprintf(str, "%d", value);
+        raha = string(str);
+    }
+    return "Kukkarosi lihavoituu " + raha + " markalla.";
+}
+bool Raha::interact(PlayerNS::Player* player)
+{
+    player->money += value;
+    return true;
 }
 
 // paska
